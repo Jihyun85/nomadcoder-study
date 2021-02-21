b6722446bcf6d35653dc16b204bee8677d0dd2e2
Jihyun An <hwiyu25@gmail.com>
2021-02-21 18:00:53 +0900

    경로 수정

--- js/background.js
+++ js/background.js
@@ -4,7 +4,7 @@ const bgAry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 const changeBackground = () => {
   let random = Math.ceil(Math.random() * 14);
-  body.style.backgroundImage = `url("https://github.com/Jihyun85/vanillaJS-challenge/blob/gh-pages/img/background/${random}.jpg?raw=true")`;
+  body.style.backgroundImage = `url("img/background/${random}.jpg")`;
 };
 
 const init = () => {
