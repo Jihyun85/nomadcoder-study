c20525388b32340a742c03357dfe76216b6ebfaf
Jihyun <hwiyu25@gmail.com>
2021-02-21 16:31:47 +0900

    이름입력 후에만 시계, todos가 보이도록 수정

--- js/greeting.js
+++ js/greeting.js
@@ -2,13 +2,22 @@ const greetingText = document.querySelector(".jsGreetingText");
 const greetingForm = document.querySelector(".jsGreetingForm");
 const greetingInput = document.querySelector(".jsGreetingInput");
 
+const clock = document.querySelector(".jsClock");
+const toDos = document.querySelector(".jsToDos");
+
 const LS_NAME = "userName";
+
+const showContent = () => {
+  clock.classList.remove("hidden");
+  toDos.classList.remove("hidden");
+};
 
 const handleSubmit = (e) => {
   e.preventDefault();
   const inputName = greetingInput.value;
   localStorage.setItem(LS_NAME, inputName);
   paintName(inputName);
+  showContent();
 };
 
 const paintName = (name) => {
@@ -22,6 +31,7 @@ const loadName = () => {
     greetingForm.addEventListener("submit", handleSubmit);
   } else {
     paintName(userName);
+    showContent();
   }
 };
 
